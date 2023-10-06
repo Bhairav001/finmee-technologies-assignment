82167ab17c9117ce537b117e20d5228b5d7ca2d0
Bhairav <bhairavgotam066@gmail.com>
2023-10-06 16:20:29 +0530

    added ntwist update

--- github/src/components/HomePage.jsx
+++ github/src/components/HomePage.jsx
@@ -1,7 +1,10 @@
 import Carousel from 'react-bootstrap/Carousel';
 import Image from 'react-bootstrap/Image';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 function HomePage() {
   return (
+    <>
     <Carousel>
       <Carousel.Item>
         <Image src='https://github.blog/wp-content/uploads/2020/12/102393310-07478b80-3f8d-11eb-84eb-392d555ebd29.png?fit=1200%2C630' alt=''/>
@@ -27,6 +30,55 @@ function HomePage() {
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
+    <div class="d-flex flex-row" style={{width:"80%", margin:"auto"}}>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://github.blog/wp-content/uploads/2023/01/1200x640.png?fit=1200%2C640 " />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://github.blog/wp-content/uploads/2023/01/1200x640.png?fit=1200%2C640 " />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://github.blog/wp-content/uploads/2023/01/1200x640.png?fit=1200%2C640 " />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://github.blog/wp-content/uploads/2023/01/1200x640.png?fit=1200%2C640 " />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    
+    </div>
+    </>
+
   );
 }
 
